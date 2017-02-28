7ddacc38de3062304964eed720ba0f770dbd1228
oweiler <weiler@predic8.de>
2017-02-28 21:50:34 +0100

    SELECT N+1 problem

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO author (id, name) VALUES (1, 'Oliver Weiler');
+INSERT INTO author (id, name) VALUES (2, 'Thomas Bayer');
+INSERT INTO author (id, name) VALUES (3, 'Tobias Polley');
 INSERT INTO post (id, title, content, author_id) VALUES (1, 'What is Spring Boot?', 'Spring Boot makes it easy TO CREATE stand - alone, production - grade Spring based Applications that you can "just run"', 1);
+INSERT INTO post (id, title, content, author_id) VALUES (2, 'What is Spring Boot?', 'Spring Boot makes it easy TO CREATE stand - alone, production - grade Spring based Applications that you can "just run"', 2);
+INSERT INTO post (id, title, content, author_id) VALUES (3, 'What is Spring Boot?', 'Spring Boot makes it easy TO CREATE stand - alone, production - grade Spring based Applications that you can "just run"', 3);
