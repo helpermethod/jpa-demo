778ea3ef2423a4f04f55eba042b2d45dba7d722f
oweiler <weiler@predic8.de>
2017-02-28 21:45:38 +0100

    Eager fetching

--- src/main/java/de/predic8/jpa/domain/Author.java
+++ src/main/java/de/predic8/jpa/domain/Author.java
@@ -1,9 +1,6 @@
 package de.predic8.jpa.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -12,7 +9,7 @@ public class Author {
     @GeneratedValue
     private Long id;
     private String name;
-    @OneToMany(mappedBy = "author")
+    @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
     private List<Post> posts;
 
     public String getName() {
